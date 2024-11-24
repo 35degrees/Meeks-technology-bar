b5a2638087a140b9f9294d8cb9e658ba9f2146f2
Sam <samhooker@gmail.com>
2024-11-24 11:43:37 -0800

    updated link

--- src/routes/+page.js
+++ src/routes/+page.js
@@ -1,11 +1,14 @@
 import Papa from 'papaparse';
 
 export const load = async ({ fetch }) => {
-	const res = await fetch('static/data.csv', {
-		headers: {
-			'Content-Type': 'text/csv'
+	const res = await fetch(
+		'https://github.com/35degrees/Meeks-technology-bar/raw/refs/heads/main/static/data.csv',
+		{
+			headers: {
+				'Content-Type': 'text/csv'
+			}
 		}
-	});
+	);
 	let csvSkills = await res.text();
 	let parsedCsvSkills = Papa.parse(csvSkills, { header: true });
 
